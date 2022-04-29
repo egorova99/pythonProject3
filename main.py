f5819efbf641a262bc217c32a146751b138d081e
egorova99 <egorova991073@gmail.com>
2022-04-29 15:24:02 +0300

    правки

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 
+
 base_path = os.getcwd()
 file_name1 = '1'
 file_name2 = '2'
@@ -9,27 +10,30 @@ full_path = os.path.join(base_path, log_dir, file_name1)
 full_path2 = os.path.join(base_path, log_dir, file_name2)
 full_path3 = os.path.join(base_path, log_dir, file_name3)
 
-class Files:
- def file_worker(encoding: str):
-   file_name = full_path
-   mode = 'r'
-   with open(file_name, mode, encoding=encoding) as file:
-     x = file.readlines()
-     log_str = f'имя файла: {file_name1}.txt\nкол-во строк: 2\n{x[0]}{x[1]}'
-     print(log_str)
-   file_name = full_path2
-   with open(file_name, mode, encoding=encoding) as file:
-     y = file.readlines()
-     log_str2 = f'имя файла: {file_name2}.txt\nкол-во строк: 1\n{y[0]}'
-     print(log_str2)
-   file_name = full_path3
-   mode = 'w'
-   with open(file_name, mode, encoding=encoding) as file:
-     log_str3 = f'{log_str}\n{log_str2}'
-     file.write(log_str3)
-     file.close()
+def file_worker(mode: str, encoding: str):
+    log_text = ''
+    file_name = full_path
+    with open(file_name, mode, encoding=encoding) as file:
+        x = file.readlines()
+        log_str1 = f'имя файла: {file_name1}.txt\nкол-во строк: 2\n{x[0]}{x[1]}'
+        print(log_str1)
+    file_name = full_path2
+    with open(file_name, mode, encoding=encoding) as file:
+        y = file.readlines()
+        log_str2 = f'имя файла: {file_name2}.txt\nкол-во строк: 1\n{y[0]}'
+        print(log_str2)
+    file_name = full_path3
+    log_text = f'{log_str1}\n{log_str2}'
+    return log_text
+
+def file_write(mode: str, encoding: str):
+    write_text = log_text
+    file_name = full_path3
+    with open(file_name, mode, encoding=encoding) as file:
+        file.write(write_text)
+        file.close()
 
 
-my_file = Files
-my_file.file_worker('utf-8')
+log_text = file_worker('r', 'utf-8')
+file_write('w', 'utf-8')
 
